a641150e678dfed7ba6a2444506a6ed493650c35
unknown <kre8mymedia@gmail.com>
2019-11-06 18:59:46 -0600

    .

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        $x = new User;
+        $x->name = 'John Doe';
+        $x->email = 'john@test.com';
+        $x->password = '$2y$10$4Ad8EX5ZqJI3EJ2zTTMDbOwkqAXsRZ/EaOOPsKVRtygz51aCcdUyC';
+        $x-save();
+
+        $x = new User;
+        $x->name = 'Harry';
+        $x->email = 'harry@test.com';
+        $x->password = 'password1=';
+        $x-save();
+
+        $x = new User;
+        $x->name = 'Mary';
+        $x->email = 'mary@test.com';
+        $x->password = 'password1=';
+        $x-save();
+    }
+}
