2b303d8b29462005f19ddad89c3efc5b25638a23
unknown <kre8mymedia@gmail.com>
2019-11-08 22:14:18 -0600

    fixed seeder problem

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,18 +18,18 @@ class DatabaseSeeder extends Seeder
         $x->name = 'John Doe';
         $x->email = 'john@test.com';
         $x->password = '$2y$10$4Ad8EX5ZqJI3EJ2zTTMDbOwkqAXsRZ/EaOOPsKVRtygz51aCcdUyC';
-        $x-save();
+        $x->save();
 
         $x = new User;
         $x->name = 'Harry';
         $x->email = 'harry@test.com';
         $x->password = 'password1=';
-        $x-save();
+        $x->save();
 
         $x = new User;
         $x->name = 'Mary';
         $x->email = 'mary@test.com';
         $x->password = 'password1=';
-        $x-save();
+        $x->save();
     }
 }
